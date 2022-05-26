1aaa018192f496caa0a8fa9ce9269c495ad659e8
hossein <hosseinmeysami.25@gmail.com>
2022-05-26 23:47:45 +0430

    feat: transfer database config to ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { ExpenseModule } from './expense/expense.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'expense-calculator',
+      host: '****',
+      port: '***',
+      username: '****',
+      password: '***',
+      database: '****',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
